3197c340bca8019194fbde40a38ecfcca39ef77f
Adityadandekar28 <adityadandekar21@gmail.com>
2026-02-20 15:00:18 +0530

    Add files via upload
    First Project of streamlit

---
+++ app.py
@@ -0,0 +1,8 @@
+st.text_input("Naam kya hai bhai")
+st.text_area(" Write something yaar")
+st.number_input("Enter a number", min_value=0, max_value=100)
+st.slider("Choose a range", 0, 100, )
+st.selectbox("Select a fruit", ["Kiwi", "Peach", "Litchi"])
+st.multiselect("Choose Toppings", ["Cheese", "Tomato", "Olves"])
+st.radio("Pick a color", ["Red", "Green", "Blue"])
+
